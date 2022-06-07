eacbc6de86337596f7716d3d3945718d194eb713
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-06-07 18:39:27 +0200

    Add slider effect for images

--- Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
+++ Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-
+  $('.bike__slider').slick({
+    arrows: false,
+    dots: true,
+    // style for animation 
+    fade: true,
+    speed: 500,
+    // autoplay for animation
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
 });
