18abf5eadaac21ce4ce7221edf0a457343846a7c
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-06-20 19:53:17 +0200

    Create correct slider

--- Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
+++ Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
@@ -1,18 +1,5 @@
 $(function () {
-  $('.bike__slider').slick({
-    arrows: false,
-    dots: true,
-    // style for animation 
-    fade: true,
-    speed: 500,
-    // autoplay for animation
-    autoplay: true,
-    autoplaySpeed: 1000,
-  });
-});
-
-$(function () {
-  $('.bikeslider-slider').slick({
+  $('.bike__slider, .slider__items').slick({
     arrows: false,
     dots: true,
     // style for animation 
