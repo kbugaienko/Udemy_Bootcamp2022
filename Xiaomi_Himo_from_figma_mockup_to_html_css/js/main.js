7452738b9452fe9692ff9129cacc2f811991ee7a
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-06-20 18:49:55 +0200

    Create section with bike slider
    * Add images
    * Add styles
    * Add js funstion for slider functionality

--- Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
+++ Xiaomi_Himo_from_figma_mockup_to_html_css/js/main.js
@@ -10,3 +10,16 @@ $(function () {
     autoplaySpeed: 1000,
   });
 });
+
+$(function () {
+  $('.bikeslider-slider').slick({
+    arrows: false,
+    dots: true,
+    // style for animation 
+    fade: true,
+    speed: 500,
+    // autoplay for animation
+    // autoplay: true,
+    // autoplaySpeed: 1000,
+  });
+});
