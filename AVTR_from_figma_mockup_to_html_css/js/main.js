93fbc78fa36b581889745be604cc667628d69bc9
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-07-02 13:04:23 +0200

    Add arrows for scrolling the slider

--- AVTR_from_figma_mockup_to_html_css/js/main.js
+++ AVTR_from_figma_mockup_to_html_css/js/main.js
@@ -2,6 +2,8 @@ $(function () {
   $('.design-slider').slick({
     // dots: true,
     slidesToShow: 4,
-    variableWidth: true
+    variableWidth: true,
+    prevArrow: '<img src="images/arrow_left.png" class="arrow arrow-left" alt="arrow-left">',
+    nextArrow: '<img src="images/arrow_right.png" class="arrow arrow-right" alt="arrow-right" />',
   });
 });
