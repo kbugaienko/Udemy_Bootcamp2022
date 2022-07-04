6d6ef4156d2c2baa5d7880e490108a89ea39b42c
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-07-04 19:27:40 +0200

    Mofify media query for design section

--- AVTR_from_figma_mockup_to_html_css/js/main.js
+++ AVTR_from_figma_mockup_to_html_css/js/main.js
@@ -5,5 +5,15 @@ $(function () {
     variableWidth: true,
     prevArrow: '<img src="images/arrow_left.png" class="arrow arrow-left" alt="arrow-left">',
     nextArrow: '<img src="images/arrow_right.png" class="arrow arrow-right" alt="arrow-right" />',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          centerMode: true,
+        }
+      }
+    ],
   });
 });
