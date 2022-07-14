ece199eb3c5ff94588ece2edf3aff12c5e57ffcc
Khrystyna Bugaienko <kr.bugaienko@gmail.com>
2022-07-14 16:35:14 +0200

    Create media qyery for strategies section
    * Change amount of images in slider via js file settings

--- Productly_from_figma_mockup_to_html_css/js/main.js
+++ Productly_from_figma_mockup_to_html_css/js/main.js
@@ -4,6 +4,22 @@ $(function () {
     slidesToScroll: 3,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1145,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 710,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
   $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list-active')
