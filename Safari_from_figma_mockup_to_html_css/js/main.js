a67369b71fd51e63ae7ad77907209fc49cdc86d6
Khrystyna <kr.bugaienko@gmail.com>
2022-08-01 14:50:30 +0200

    Create slider of photogallery section
    * Add 6 images for slider
    * Create a slider of 2 pages
    * Using Magnific Popup add popup effect for slide element
    * Using Slick add prev and next buttons in svg format to the page
    * Useing 'position: absolute' position this buttons on the top-right of the
      block
    * Add styles

--- Safari_from_figma_mockup_to_html_css/js/main.js
+++ Safari_from_figma_mockup_to_html_css/js/main.js
@@ -8,4 +8,19 @@ $(function(){
 
 		fixedContentPos: false
 	});
+	$('.photogallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/slide_left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/slide_right.svg" alt=""></button>',
+	});
+	$('.photogallery__item-inner').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+	});
 });
