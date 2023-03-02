dd0d0434af0506263242019d79a55fb7f8d0ef81
Khrystyna <kr.bugaienko@gmail.com>
2023-03-02 18:19:40 +0100

    Adding basic css style

--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className='expense-item'>
       <div>April 01 2023</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$297.99</div>
+      <div className='expense-item__description'>
+        <h2 className='expense-item h2'>Car Insurance</h2>
+        <div className='expense-item__price'>$297.99</div>
       </div>
     </div>
   ); 
