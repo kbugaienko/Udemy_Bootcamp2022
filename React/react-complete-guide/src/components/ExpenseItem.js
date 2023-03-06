007efae8ce35aae79dd727ca465cc1e09346b354
Khrystyna <kr.bugaienko@gmail.com>
2023-03-06 12:47:26 +0100

    React: add 'normal' JS logic to cpmponents

--- React/react-complete-guide/src/components/ExpenseItem.js
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,9 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
+  const month = props.data.toLocaleString('en-US', {month: 'long'});
+  const day = props.data.toLocaleString('en-US', {day: 'numeric'});
+  const year = props.data.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{props.data.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2 className='expense-item h2'>{props.title}</h2>
         <div className='expense-item__price'>{props.price}</div>
