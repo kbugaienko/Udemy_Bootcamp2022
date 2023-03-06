1510ad418c9e057bb987aa31b51fdd957e99ff76
Khrystyna <kr.bugaienko@gmail.com>
2023-03-06 15:10:31 +0100

    React: Practice task
    * Create a new component that is responsible for displaying expenses
    * Add multiple ExpenseItem components in that component
    * Keep the expenses data in the App component and pass that data into the newly created component

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
   // The same in JS
@@ -37,26 +37,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        price={expenses[1].price}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        price={expenses[2].price}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        price={expenses[3].price}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
