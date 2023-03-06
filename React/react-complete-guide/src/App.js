80925837c73d13a29ede786823732cdcc6ffe60c
Khrystyna <kr.bugaienko@gmail.com>
2023-03-06 14:22:00 +0100

    React: Split components into multiple components

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -11,25 +11,25 @@ function App() {
       id: "e1",
       title: "Car insurance",
       price: 297.99,
-      data: new Date(2021, 2, 28),
+      date: new Date(2021, 2, 28),
     },
     {
       id: "e2",
       title: "Toilet paper",
       price: 299.99,
-      data: new Date(2021, 3, 31),
+      date: new Date(2021, 3, 31),
     },
     {
       id: "e3",
       title: "Kitchen table",
       price: 355.99,
-      data: new Date(2022, 5, 11),
+      date: new Date(2022, 5, 11),
     },
     {
       id: "e4",
       title: "New desk(wooden)",
       price: 453.99,
-      data: new Date(2022, 7, 15),
+      date: new Date(2022, 7, 15),
     },
   ];
 
@@ -40,22 +40,22 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         price={expenses[0].price}
-        data={expenses[0].data}
+        date={expenses[0].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         price={expenses[1].price}
-        data={expenses[1].data}
+        date={expenses[1].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         price={expenses[2].price}
-        data={expenses[2].data}
+        date={expenses[2].date}
       ></ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         price={expenses[3].price}
-        data={expenses[3].data}
+        date={expenses[3].date}
       ></ExpenseItem>
     </div>
   );
