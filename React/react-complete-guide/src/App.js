0e6eccf2cf299ee510ec3aa1c3a04c763f690a28
Khrystyna <kr.bugaienko@gmail.com>
2023-03-15 16:11:19 +0100

    React: Using stateful lists

--- React/react-complete-guide/src/App.js
+++ React/react-complete-guide/src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car insurance",
+    price: 297.99,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Toilet paper",
+    price: 299.99,
+    date: new Date(2021, 3, 31),
+  },
+  {
+    id: "e3",
+    title: "Kitchen table",
+    price: 355.99,
+    date: new Date(2022, 5, 11),
+  },
+  {
+    id: "e4",
+    title: "New desk(wooden)",
+    price: 453.99,
+    date: new Date(2022, 7, 15),
+  },
+];
 
 const App = () => {
   // The same in JS
@@ -9,36 +36,13 @@ const App = () => {
   // parag.textContent = 'This is also visible!';
   // document.getElementById('root').append(parag);
 
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car insurance",
-      price: 297.99,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Toilet paper",
-      price: 299.99,
-      date: new Date(2021, 3, 31),
-    },
-    {
-      id: "e3",
-      title: "Kitchen table",
-      price: 355.99,
-      date: new Date(2022, 5, 11),
-    },
-    {
-      id: "e4",
-      title: "New desk(wooden)",
-      price: 453.99,
-      date: new Date(2022, 7, 15),
-    },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    // setExpenses([expense, ...expenses]);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
