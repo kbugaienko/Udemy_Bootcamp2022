85d1992d99555ac1a7f02f9488991024d5cfa787
Khrystyna <kr.bugaienko@gmail.com>
2023-03-15 20:33:54 +0100

    React: Conditional content
    * Add cancel button
    * Add the ability to open or collapse the window for adding an entry by clicking.

--- React/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ React/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [isEditting, setIsEditting] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditting(false);
   };
 
+  const startEdittingHandler = () => {
+    setIsEditting(true);
+  };
+
+  const stopEdittingHandler = () => {
+    setIsEditting(false);
+  };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditting && <button onClick={startEdittingHandler}>Add New Expense</button>}
+      {isEditting && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEdittingHandler} />}
     </div>
   );
 }
