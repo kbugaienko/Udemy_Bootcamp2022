72090749dff701aacabeb2870c0ca662e4bd87b0
Khrystyna <kr.bugaienko@gmail.com>
2023-03-20 18:16:44 +0100

    Creayte a chart

--- React/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ React/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -12,7 +12,7 @@ const ExpenseItem = (props) => {
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
           <h2 className='expense-item h2'>{props.title}</h2>
-          <div className='expense-item__price'>{props.price}</div>
+          <div className='expense-item__price'>${props.price}</div>
         </div>
       </Card>
     </li>
