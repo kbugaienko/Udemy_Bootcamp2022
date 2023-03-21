1669d14e397515686be8b078ed7e2627f2a52533
Khrystyna <kr.bugaienko@gmail.com>
2023-03-21 16:59:33 +0100

    React: Wrap up and next steps
    * Fill chart bars

--- React/react-complete-guide/src/components/Chart/Chart.js
+++ React/react-complete-guide/src/components/Chart/Chart.js
@@ -5,13 +5,17 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  const totalMax = Math.max(...dataPointValues);
+  
+  console.log(dataPointValues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMax}
           label={dataPoint.label}
         />
       ))}
--- React/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ React/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -57,7 +57,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      price: enteredPrice,
+      price: +enteredPrice,
       date: new Date(enteredDate),
     };
 
