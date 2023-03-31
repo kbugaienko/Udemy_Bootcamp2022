1fa15e632ab9618359a604d60fa9dff0518bb467
Khrystyna <kr.bugaienko@gmail.com>
2023-03-31 21:51:25 +0200

    Add validation and reseting logic

--- React/react-practice-project/src/components/Users/AddUser.js
+++ React/react-practice-project/src/components/Users/AddUser.js
@@ -10,7 +10,18 @@ const AddUser = props => {
 
   const addUserHandler = event => {
     event.preventDefault();
+
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = event => {
@@ -25,9 +36,9 @@ const AddUser = props => {
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type='text' onChange={usernameChangeHandler}></input>
+        <input id="username" type='text' onChange={usernameChangeHandler} value={enteredUsername}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type='number' onChange={ageChangeHandler}></input>
+        <input id="age" type='number' onChange={ageChangeHandler} value={enteredAge}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
