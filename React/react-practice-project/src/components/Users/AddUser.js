5a2ecd0ed3717a602048df3d79981018000ad341
Khrystyna <kr.bugaienko@gmail.com>
2023-04-03 15:29:33 +0200

    Add a Wrapper component

--- React/react-practice-project/src/components/Users/AddUser.js
+++ React/react-practice-project/src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card';
 import Button from "../UI/Button";
 import styles from './AddUser.module.css';
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -48,7 +49,7 @@ const AddUser = props => {
   };
 
   return (
-    <div>
+    <Wrapper>
       { error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} /> }
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
@@ -59,7 +60,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
