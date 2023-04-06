05f3a3135c0a9cb7180e0f6af6556965a8aac83a
Khrystyna <kr.bugaienko@gmail.com>
2023-04-06 16:57:12 +0200

    Add unique keys

--- React/react-todolist-tutorial/src/App.js
+++ React/react-todolist-tutorial/src/App.js
@@ -4,7 +4,7 @@ import Todo from "./components/Todo";
 
 const App = props => {
   const taskList = props.tasks.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} />
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
   ));
 
   return (
