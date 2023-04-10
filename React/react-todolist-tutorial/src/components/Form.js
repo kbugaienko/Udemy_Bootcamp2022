5dd9045bb6d35604f31bb38a36e62b63e1888497
Khrystyna <kr.bugaienko@gmail.com>
2023-04-10 19:35:39 +0200

    Use useState Hook

--- React/react-todolist-tutorial/src/components/Form.js
+++ React/react-todolist-tutorial/src/components/Form.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = props => {
+  const [name, setName] = useState('');
+
   const handlerSubmit = event => {
     event.preventDefault();
-    props.addTask('Say Hello');
+    props.addTask(name);
+    setName('');
+  };
+
+  const changeHandler = e => {
+    setName(e.target.value);
   };
 
   return (
@@ -19,6 +26,8 @@ const Form = props => {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={changeHandler}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
