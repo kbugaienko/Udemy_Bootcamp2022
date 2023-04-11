60c84afe6fc15ef8391b1ae8eec1318c1da17320
Khrystyna <kr.bugaienko@gmail.com>
2023-04-11 14:16:48 +0200

    Deleting a task

--- React/react-todolist-tutorial/src/App.js
+++ React/react-todolist-tutorial/src/App.js
@@ -6,38 +6,49 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-const App = props => {
+const App = (props) => {
   const [tasks, setTasks] = useState(props.tasks);
 
-  const toggleTaskCompleted = id => {
+  const toggleTaskCompleted = (id) => {
     const updatedTasks = tasks.map((task) => {
       // if this task has the same ID as the edited task
       if (id === task.id) {
         // use object spread to make a new object
         // whose `completed` prop has been inverted
-        return {...task, completed: !task.completed}
+        return { ...task, completed: !task.completed };
       }
       return task;
     });
     setTasks(updatedTasks);
-  }  
+  };
+
+  const deleteTask = (id) => {
+    console.log(id);
+  };
 
   const taskList = tasks.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} />
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
   ));
 
-  const addTask = name => {
+  const addTask = (name) => {
     const newTask = {
       id: `todo-${nanoid()}`,
       name,
-      completed: false
+      completed: false,
     };
     setTasks([...tasks, newTask]);
   };
-  
-  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -48,8 +59,10 @@ const App = props => {
         <FilterButton />
       </div>
       <h2 id="list-heading">{headingText}</h2>
-      <ul className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
         {taskList}
         {/* <Todo name='Eat' completed={true} id='todo-0' />
         <Todo name='Sleep' completed={false} id='todo-1' />
@@ -57,6 +70,6 @@ const App = props => {
       </ul>
     </div>
   );
-}
+};
 
 export default App;
