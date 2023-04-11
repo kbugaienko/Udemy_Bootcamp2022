34570dbb5f1851284c2e0509e9d26ce3e8bc0e6b
Khrystyna <kr.bugaienko@gmail.com>
2023-04-11 18:09:57 +0200

    Filtering buttons

--- React/react-todolist-tutorial/src/App.js
+++ React/react-todolist-tutorial/src/App.js
@@ -6,8 +6,17 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 const App = (props) => {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
   const toggleTaskCompleted = (id) => {
     const updatedTasks = tasks.map((task) => {
@@ -50,6 +59,15 @@ const App = (props) => {
     />
   ));
 
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
   const addTask = (name) => {
     const newTask = {
       id: `todo-${nanoid()}`,
@@ -67,9 +85,7 @@ const App = (props) => {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
