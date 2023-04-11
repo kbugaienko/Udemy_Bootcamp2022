9ae23378dddd1a4769144e588c4354fda4d0edd5
Khrystyna <kr.bugaienko@gmail.com>
2023-04-11 17:18:35 +0200

    UI for editing, conditional rendering

--- React/react-todolist-tutorial/src/components/Todo.js
+++ React/react-todolist-tutorial/src/components/Todo.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = (props) => {
-  return (
-    <li className="todo stack-small">
+  const [isEditing, setEditing] = useState();
+
+  const editingTemplate = (
+    <form className="stack-small">
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input id={props.id} className="todo-text" type="text" />
+      </div>
+      <div className="btn-group">
+        <button type="button" className="btn todo-cancel" onClick={() => setEditing(false)}>
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
+    <div className="stack-small">
       <div className="c-cb">
         <input
           id={props.id}
@@ -10,12 +33,12 @@ const Todo = (props) => {
           defaultChecked={props.completed}
           onChange={() => props.toggleTaskCompleted(props.id)}
         />
-        <label className="todo-label" htmlFor="todo-0">
+        <label className="todo-label" htmlFor={props.id}>
           {props.name}
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
@@ -26,6 +49,12 @@ const Todo = (props) => {
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
+    </div>
+  );
+
+  return (
+    <li className="todo stack-small">
+      {isEditing ? editingTemplate : viewTemplate}
     </li>
   );
 };
