208aebd1ab6185e14af2412d9913203f82184faa
Khrystyna <kr.bugaienko@gmail.com>
2023-04-26 22:31:45 +0200

    Add FontAwesome

--- React/react-carousel/src/App.js
+++ React/react-carousel/src/App.js
@@ -1,10 +1,49 @@
+import React, { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
+const images = [
+  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+];
+
+const App = () => {
+  const [current, setCurrent] = useState(0);
+
+  const nextSlide = () => {
+    setCurrent(current === images.length-1 ? 0 : current+1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? images.length-1 : current-1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <h2>Carousel</h2>
+      <div className='slider'>
+        <div className='left-arrow' onClick={prevSlide}>
+          <FontAwesomeIcon icon={faArrowLeft} size="lg" style={{color: "#e66100",}} />
+          {/* <FontAwesomeIcon icon="fa-solid fa-arrow-left" /> */}
+        </div>
+        <div className='right-arrow' onClick={nextSlide}>
+          <FontAwesomeIcon icon={faArrowRight} size="lg " style={{color: "#e66100",}} />
+          {/* <FontAwesomeIcon icon="fa-solid fa-arrow-right" /> */}
+        </div>
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className='slide'>
+                <img src={image} alt='images' />
+              </div>
+            )
+        )}
+      </div>
     </div>
   );
 }
