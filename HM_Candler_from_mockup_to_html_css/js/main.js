987d771f0abf82c98c03cc25c9fdc87901567b67
Kristina Bugaienko <kr.bugaienko@gmail.com>
2023-09-10 16:23:10 +0200

    Create feedback section
    * Add styles
    * Create slider by using Swiper library

--- HM_Candler_from_mockup_to_html_css/js/main.js
+++ HM_Candler_from_mockup_to_html_css/js/main.js
@@ -4,3 +4,11 @@ const menuMobile = document.querySelector('.menu__header-list');
 menuBtn.addEventListener('click', ()=> {
     menuMobile.classList.toggle('menu--open');
 });
+
+const swiperOne = new Swiper('.feedback__slider', {
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
