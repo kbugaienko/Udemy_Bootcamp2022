434f20a7ec88098139778be670ad3d6603e8ce77
Kristina Bugaienko <kr.bugaienko@gmail.com>
2023-09-10 17:31:43 +0200

    Create certificates section
    * Create slider by using Swiper
    * Add styles

--- HM_Candler_from_mockup_to_html_css/js/main.js
+++ HM_Candler_from_mockup_to_html_css/js/main.js
@@ -2,13 +2,23 @@ const menuBtn = document.querySelector('.menu__btn');
 const menuMobile = document.querySelector('.menu__header-list');
 
 menuBtn.addEventListener('click', ()=> {
-    menuMobile.classList.toggle('menu--open');
+  menuMobile.classList.toggle('menu--open');
 });
 
 const swiperOne = new Swiper('.feedback__slider', {
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  });
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
